3ec88644463fa0c5449973576ef257cb3b31be41
Orest Zaluzhnyi <inexes22@gmail.com>
2024-04-19 17:45:54 +0200

    created home work 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,73 @@
-
+import css from '../App/App.module.css'
+import Options from '../Options/Options';
+import Feedback from '../Feedback/Feedback';
+import Description from '../Description/Description';
+import Advertisement from '../Advertisement/Advertisement'
+import { useState, useEffect } from 'react';
 
 export default function App() {
+  const [feedbackCount, setFeedbackCount] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  useEffect(() => {
+    const savedFeedback = localStorage.getItem('feedbackCount');
+    if (savedFeedback !== null) {
+      return setFeedbackCount(JSON.parse(savedFeedback));
+    }
+    return 0;
+  }, []);
+
+
+  useEffect(() => {
+    localStorage.setItem('feedbackCount', JSON.stringify(feedbackCount));
+  }, [feedbackCount]);
+
+
+  const updateFeedback = feedbackType => {
+    setFeedbackCount(prevFeedback => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+
+  const totalFeedback =
+    feedbackCount.good + feedbackCount.neutral + feedbackCount.bad;
+
+
+  const positiveFeedback = Math.round(
+    (feedbackCount.good / totalFeedback) * 100
+  );
+
+  const resetFeedback = () => {
+    setFeedbackCount({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
 
   return (
     <>
-      <h1>Orest</h1>
+      <Description />
+
+      <Options
+        updateFeedback={updateFeedback}
+        resetFeedback={resetFeedback}
+        totalFeedback={totalFeedback}
+      />
+      {totalFeedback ? (
+        <Feedback
+          feedback={feedbackCount}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Advertisement/>
+      )}
     </>
-  )
+  );
 }
-
-
--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,1 +1,52 @@
-import css from './Options.module.css'
+import css from '../Options/Options.module.css'
+
+export default function Options({
+  updateFeedback,
+  resetFeedback,
+  totalFeedback,
+}) {
+  return (
+    <div>
+      <ul className={css.list}>
+        <li>
+          <button
+            type="button"
+            className={css.button}
+            onClick={() => updateFeedback('good')}
+          >
+            Good
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className={css.button}
+            onClick={() => updateFeedback('neutral')}
+          >
+            Neutral
+          </button>
+        </li>
+        <li>
+          <button
+            type="button"
+            className={css.button}
+            onClick={() => updateFeedback('bad')}
+          >
+            Bad
+          </button>
+        </li>
+        <li>
+          {totalFeedback !== 0 && (
+            <button
+              type="button"
+              className={css.button}
+              onClick={resetFeedback}
+            >
+              Reset
+            </button>
+          )}
+        </li>
+      </ul>
+    </div>
+  );
+}
